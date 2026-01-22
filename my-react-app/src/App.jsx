6078acb6915e9ffb7452cb917b58245082242526
Ruben Emanuel Cruz Gacia <240185@utxicotepec.edu.mx>
2026-01-22 14:20:09 -0600

    ADD: Se agregaron listas y funciones a app.jsx de React

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -4,9 +4,58 @@ function App(){
   <h1>5A EVND</h1>
   <h2>Alumno</h2>
   <h3>Ruben Emanuel cruz Garcia</h3>
-  <h4>Profesor:</h4>
-  <h4>M.T.I. Ricardo Luna Santos</h4>
+  <UserComponent />
+  <ProfileComponent/>
+  <FeedComponent/>
   </div>
   )
+}
+function UserComponent(){
+  const nombre = 'hot';
+  const apellidos = 'dog';
+  const nombreCompleto = <h3>El nombre es: {nombre} y sus apellidos {apellidos}</h3>; 
+  return <h3>User Component {nombreCompleto}</h3>;
+}
+function ProfileComponent(){
+  const users = [
+    { id: 1, name:'Ruben', role: 'web Devolper'},
+    { id: 1, name:'Emanuel', role: 'web Devolper'},
+    { id: 1, name:'Hotdog', role: 'Team Leader'}
+  ]
+  return (
+    <>
+    <p>Lista de usuarios del sistema</p>
+    <ul>
+      {
+      users.map(function(user, index){
+      return (
+        <li key={index}>{user.name} es un {user.role} </li>
+      )
+    })
+    }
+    </ul>
+  </>
+  );
+}
+function FeedComponent(){
+  const items = [
+    { id: 1, name:'cemento', role: 'Material General'},
+    { id: 1, name:'block', role: 'Material Paredes'},
+    { id: 1, name:'HotDog', role: 'Alimento'}
+  ]
+  return (
+  <>
+  <h2 className="text-lowercase">Feed Component</h2>
+  <ul>
+    {
+    items.map(function(item, index){
+      return (
+        <li key={index}>{item.name} es para {item.role}</li>
+      )
+    }
+  )}
+  </ul>
+  </>
+  );
 }
 export default App
